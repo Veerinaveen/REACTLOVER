f2eedb97bd6fb8b6c2b8cca3622dbbecfe244682
Veerinaveen <tonystarkstarktony8542@gmail.com>
2023-11-01 11:08:32 +0530

    myApp

---
+++ my-app/src/App.js
@@ -0,0 +1,26 @@
+
+import './App.css';
+import './Hari.css';
+import Lee from './Lee';
+
+
+const App = () => {
+  
+  return (
+    <div className="App">
+        <h2>red</h2>
+        love
+      <Lee />
+    <div className="amma">
+      naveen
+      <button>click</button>
+      <Lee />
+    </div>
+
+
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ my-app/src/Lee.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Lee = () => {
+  return (
+    <div>
+      <h1>leeeeeee</h1>
+    </div>
+  )
+}
+
+export default Lee
